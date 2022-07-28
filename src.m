b843a76920c2dde27985cccd3fdb45506f8ce912
H-Alexa <54031572+H-Alexa@users.noreply.github.com>
2022-07-28 20:44:23 +0600

    Some changes toward display messages

--- src.m
+++ src.m
@@ -1,17 +1,20 @@
 %%Fuzzy traffic control%%
 f=readfis('Vehicle protection.fis')
 a = input('Vehicle speed (km/h): ');
-g=evalfis([a,0,0],f);
+g=evalfis([a,0,1],f);
 if g<1
-    disp('The vehicle is stopped and necessary steps are taken');
-else
+    disp('***Message to driver is sent***');
+    disp('***Message to police including current location is sent***');
+    disp('***Control room is informed and driver is charged against high speed***');
     
     b = input('Is alcohol present (0 for no, 1 for yes): ');
     c = input('Is the driver physically fit? (0 for no, 1 for yes): ');
     g=evalfis([a,b,c],f);
     if g<1
-        disp('The vehicle is stopped and necessary steps are taken')
+        disp('***The vehicle is stopped***')
     else
-        disp('The vehicle is good to go')
+        disp('***Other things are okay***')
     end
+else
+    disp('***The vehicle is good to go***')
 end
